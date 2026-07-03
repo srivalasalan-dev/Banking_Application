bbb49e74cf836cc2e494e3db5ca0b1e269b33df7
srivalasalan-dev <srivalasalan.manikandan@verinite.com>
2026-07-03 17:53:04 +0530

    Removed redundant HTTP Basic auth, rely on JWT filter only

--- auth-service/src/main/java/com/banking/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/banking/auth_service/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -39,9 +38,7 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider)
 
                 .addFilterBefore(jwtFilter,
-                        UsernamePasswordAuthenticationFilter.class)
-
-                .httpBasic(Customizer.withDefaults());
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
